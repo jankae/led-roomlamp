18d4eda6b52132a68de95c40cea0dbba5a89fd5d
Jan Käberich <kaeberic@ibr.cs.tu-bs.de>
2017-01-04 12:51:05 +0100

    some fixes + toggle macro added

--- software/leddriver/channels.h
+++ software/leddriver/channels.h
@@ -12,14 +12,17 @@
  * @{
  */
 #define CH1_DDR			DDRA
+#define CH1_PIN			PINA
 #define CH1_PORT		PORTA
 #define CH1_BIT			PA2
 
 #define CH2_DDR			DDRA
+#define CH2_PIN			PINA
 #define CH2_PORT		PORTA
 #define CH2_BIT			PA1
 
 #define CH3_DDR			DDRB
+#define CH3_PIN			PINB
 #define CH3_PORT		PORTB
 #define CH3_BIT			PB2
 /** @} */
@@ -30,12 +33,15 @@
  */
 #define CH1_On()		CH1_PORT |= (1<<CH1_BIT)
 #define CH1_Off()		CH1_PORT &= ~(1<<CH1_BIT)
+#define CH1_Toggle()	CH1_PIN |= (1<<CH1_BIT)
 
-#define CH2_On()		CH1_PORT |= (1<<CH1_BIT)
-#define CH2_Off()		CH1_PORT &= ~(1<<CH1_BIT)
+#define CH2_On()		CH2_PORT |= (1<<CH2_BIT)
+#define CH2_Off()		CH2_PORT &= ~(1<<CH2_BIT)
+#define CH2_Toggle()	CH2_PIN |= (1<<CH2_BIT)
 
-#define CH3_On()		CH1_PORT |= (1<<CH1_BIT)
-#define CH4_Off()		CH1_PORT &= ~(1<<CH1_BIT)
+#define CH3_On()		CH3_PORT |= (1<<CH3_BIT)
+#define CH3_Off()		CH3_PORT &= ~(1<<CH3_BIT)
+#define CH3_Toggle()	CH3_PIN |= (1<<CH3_BIT)
 /** @} */
 
 /**
